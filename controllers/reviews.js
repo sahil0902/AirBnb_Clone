45854457b9f5faa40afcf3f4da386c77e60b44c6
Muhammad Sahil <muhammadsahil757@gmail.com>
2024-02-24 23:52:20 +0000

    Fixed minor error which was causing reviews to failed

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,27 +2,31 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
 //create Route
+
 module.exports.createReview = async (req, res) => {
-    try {
+  try {
       console.log(req.params.id);
       let listing = await Listing.findById(req.params.id);
       let newReview = new Review(req.body.review);
       newReview.author = req.user._id; //set the user id as author of this review
       console.log(newReview);
       listing.reviews.push(newReview);
-      await newReview.save().catch((e)=>{req.flash("error", e.message)
-       res.redirect(`/listings/${req.params.id}`)}); 
-      await listing.save().catch((e)=>{req.flash("error", e.message)
-      res.redirect(`/listings/${req.params.id}`)});
+      await newReview.save().catch((e) => {
+          req.flash("error", e.message);
+          return res.redirect(`/listings/${req.params.id}`);
+      });
+      await listing.save().catch((e) => {
+          req.flash("error", e.message);
+          return res.redirect(`/listings/${req.params.id}`);
+      });
       req.flash("success", "Your review has been added!");
       console.log("new rev saved");
       return res.redirect(`/listings/${req.params.id}`);
-    } catch (e) {
+  } catch (e) {
       req.flash("error", e.message);
       return res.redirect(`/listings/${req.params.id}`);
-    }
-  };
-
+  }
+};
   //delete route
   module.exports.deleteReview = async (req, res) => {
     let { id, reviewId } = req.params;
